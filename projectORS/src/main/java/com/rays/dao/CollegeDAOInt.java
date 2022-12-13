175344f93d03531da4aec10d4cf97eb49899ebe2
bhupendra patidar <bhupendra2000.bp@gmail.com>
2022-12-13 13:56:38 +0530

    success

---
+++ projectORS/src/main/java/com/rays/dao/CollegeDAOInt.java
@@ -0,0 +1,12 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.CollegeDTO;
+
+/**
+ * College DAO interface.
+ * @author bhupendra patidar
+ * 
+ */
+public interface CollegeDAOInt extends BaseDAOInt<CollegeDTO> {
+}
---
+++ projectORS/src/main/java/com/rays/exception/DatabaseException.java
@@ -0,0 +1,19 @@
+package com.rays.exception;
+
+/**
+ * DatabaseException is propogated by DAO classes when an unhandled Database
+ * exception occurred
+ * 
+ * @author bhupendra patidar
+ */
+public class DatabaseException extends RuntimeException {
+
+	/**
+	 * @param msg
+	 *            : Error message
+	 */
+	public DatabaseException(String msg) {
+		super(msg);
+	}
+
+}
